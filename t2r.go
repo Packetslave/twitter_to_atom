215107dc8d5d750435f3dba23ca600a17e060dac
Brian Landers <brian@packetslave.com>
2013-05-04 16:44:57 -0700

    parse the tweets and get ID/Text

--- t2r.go
+++ t2r.go
@@ -2,36 +2,51 @@ package main
 
 import (
 	"encoding/json"
-	// "fmt"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
 
-func main() {
-	resp, err := http.Get("http://search.twitter.com/search.json?q=from%3Araymondh")
+const Twitter = "http://search.twitter.com/search.json?q=from%%3A%s"
+
+type Tweet struct {
+	Id   float64
+	Text string
+}
+
+type Result struct {
+	Completed_in float64
+	Results      []Tweet
+}
+
+func GetTweets(user string) Result {
+	resp, err := http.Get(fmt.Sprintf(Twitter, user))
+	defer resp.Body.Close()
 
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	type Message struct {
-		Completed_in         float64
-		Max_id, Min_id, Page float64
-		Query                string
-		Results              []interface{}
-	}
-
-	var m Message
+	var m Result
 	err = json.Unmarshal(body, &m)
 
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	return m
+}
+
+func main() {
+	result := GetTweets("raymondh")
+
+	for _, value := range result.Results {
+		fmt.Printf("%s: %s\n", value.Id, value.Text)
 	}
 }
